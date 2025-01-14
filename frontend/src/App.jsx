3228aa21206647ec9ddd95ad600c592bbb5a9660
Aroncho <barriosarom@gmail.com>
2025-01-14 12:14:22 -0300

    protect routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import NavBar from './components/NavBar/NavBar'
-import Fab from '@mui/material/Fab'
-import './App.css'
-import MyShoppingList from './pages/myShoppingList/myShoppingList'
-import Users from './pages/Users/Users'
-import Login from './pages/Login/Login'
-import Register from './pages/Register/Register'
-import { AuthProvider } from './context/AuthContext'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+import MyShoppingList from "./pages/myShoppingList/myShoppingList";
+import ProtectedRoutes from "./ProtectedRoutes/ProtectedRoutes";
+import Users from "./pages/Users/Users";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shoppinglist" element={<MyShoppingList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-    </Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/shoppinglist" element={<MyShoppingList />} />
+            <Route path="/users" element={<Users />} />
+          </Route>
+        </Routes>
+      </Router>
     </AuthProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
